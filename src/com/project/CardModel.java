9cf0ff4f82474162e267b7433581716692d17419
Rick <shariq1989@gmail.com>
2019-02-01 21:37:56 -0700

    Added object model

---
+++ src/com/project/CardModel.java
@@ -0,0 +1,24 @@
+package com.project;
+
+class CardModel {
+
+    private String rank;
+    private String suit;
+
+    String getRank() {
+        return rank;
+    }
+
+    void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    String getSuit() {
+        return suit;
+    }
+
+    void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+}
--- src/com/project/DeckShufflerApp.java
+++ src/com/project/DeckShufflerApp.java
@@ -5,39 +5,45 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * This class prints to console a shuffled 52 card deck
+ * This class prints to console a list of cards from a shuffled 52 card deck
  */
-public class deckShufflerApp {
+public class DeckShufflerApp {
     public static void main(String[] args) {
         shuffleDeck();
     }
 
     /**
-     * Prints an array of strings representing a shuffled 52 deck of cards.
+     * Prints strings representing a shuffled 52 card deck
      */
     private static void shuffleDeck() {
         String[] cardRanks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
         String[] cardSuits = {"Clubs", "Diamonds", "Hearts", "Spades"};
-        List<String> cardsToBeProcessed = new ArrayList<>();
-        List<String> shuffledDeck = new ArrayList<>();
 
-        //Build a 52 card sorted deck
+        List<CardModel> cardsToBeProcessed = new ArrayList<>();
+        List<CardModel> shuffledDeck = new ArrayList<>();
+
+        // Build a sorted 52 card deck
         for (String suit : cardSuits) {
             for (String rank : cardRanks) {
-                cardsToBeProcessed.add(rank + " of " + suit + " ");
+                CardModel newCard = new CardModel();
+                newCard.setRank(rank);
+                newCard.setSuit(suit);
+                cardsToBeProcessed.add(newCard);
             }
         }
 
-        //While there are still cards to be processed
+        // While there are still cards to be processed
         while (cardsToBeProcessed.size() != 0) {
-            //Add a random card from the sorted deck to the new deck
+            // Add a random card from the sorted deck to the new deck
             int randomCard = randomize(cardsToBeProcessed.size());
             shuffledDeck.add(cardsToBeProcessed.get(randomCard));
-            //Remove the card from the sorted deck
+            // Remove the card from the sorted deck
             cardsToBeProcessed.remove(randomCard);
         }
-        //Print the shuffled deck
-        System.out.println(shuffledDeck);
+        // Print the shuffled deck
+        for (CardModel card : shuffledDeck) {
+            System.out.println(card.getRank() + " of " + card.getSuit());
+        }
     }
 
     /**
